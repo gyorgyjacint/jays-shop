a96f3d4e79bb898b789c5f75c61daf8584798917
gyorgyjacint <jicant@gmail.com>
2024-01-08 15:56:35 +0100

    AppContext added

---
+++ Server/Jaysbe/Data/AppContext.cs
@@ -0,0 +1,29 @@
+﻿using Jaysbe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jaysbe.Data;
+
+public class AppContext : DbContext
+{
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<SubCategory> SubCategories { get; set; }
+
+    public AppContext(DbContextOptions<AppContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+        /*
+        modelBuilder.Entity("Jaysbe.Models.Category", b =>
+        {
+            b.Property<decimal>("Price")
+                .HasColumnType(typeof(decimal));
+        }
+        */  
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
