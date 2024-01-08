ad968d85fc8f159eccf708f484a012135bf4fffc
gyorgyjacint <jicant@gmail.com>
2024-01-08 11:38:51 +0100

    Empty projects created

---
+++ Server/Jaysbe/Controllers/DummyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jaysbe.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class DummyController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Dummy1()
+    {
+        return Ok("ok");
+    }
+}
