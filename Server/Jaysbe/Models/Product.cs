524680912c660e3df18432295d01c49cddbef898
gyorgyjacint <jicant@gmail.com>
2024-01-10 17:41:52 +0100

    rename: Model Item -> Product

--- Server/Jaysbe/Models/Product.cs
+++ Server/Jaysbe/Models/Product.cs
@@ -4,10 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Jaysbe.Models;
 
-public class Item
+public class Product
 {
-    public Guid ItemId { get; init; }
-    public int ItemNumber { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid? ProductId { get; init; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public uint ProductNumber { get; set; }
     
     [MaxLength(70)]
     public string Name { get; set; }
@@ -19,16 +22,16 @@ public class Item
     [Precision(16, 2)]
     public decimal? DiscountPrice { get; set; }
     public int Quantity { get; set; }
-    
+
     [MaxLength(500)]
     public string Description { get; set; }
     [MaxLength(40)]
     public string? Color { get; set; }
-    public IEnumerable<ItemDescOption> ItemDescriptions { get; set; }
+    public IEnumerable<ProductDescOption>? ProductDescriptions { get; set; }
     
     [MaxLength(150)]
     public string ThumbnailUrl { get; set; }
-    public IEnumerable<string> PicturesUrls { get; set; }
+    public IList<string>? PicturesUrls { get; set; }
     
-    public Category Category { get; set; }
+    public Category? Category { get; set; }
 }
---
+++ Server/Jaysbe/Models/ProductDescOption.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jaysbe.Models;
+
+
+public record ProductDescOption
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid? ProductDescOptionId { get; init; }
+    
+    [MaxLength(70)]
+    public string Name { get; set; }
+    [MaxLength(150)]
+    public string Option { get; set; }
+}
