f4d30902161dae3ae07875be9c766a76c52fb0f4
gyorgyjacint <jicant@gmail.com>
2024-01-10 17:43:55 +0100

    fix: AppContext renamed to AppDbContext due to  ambiguous reference

---
+++ Server/Jaysbe/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Jaysbe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jaysbe.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<SubCategory>? SubCategories { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
