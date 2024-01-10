6bf1892c07c617a394355bd60f179834aa58905a
gyorgyjacint <jicant@gmail.com>
2024-01-10 17:47:56 +0100

    add: CategoryController and ProductController

---
+++ Server/Jaysbe/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Jaysbe.Data;
+using Jaysbe.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jaysbe.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class CategoryController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    private readonly ILogger<CategoryController> _logger;
+    
+    public CategoryController(AppDbContext context, ILogger<CategoryController> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Post(Category category)
+    {
+        _logger.LogInformation($"{nameof(Post)}, called.");
+        
+        var entity = await _context.Categories.AddAsync(category);
+        await _context.SaveChangesAsync();
+
+        _logger.LogInformation($"{nameof(Post)}, Category entity added to database.");
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<Category> GetFirst()
+    {
+        var item = await _context.Categories.FirstAsync();
+        return item;
+    }
+}
---
+++ Server/Jaysbe/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Jaysbe.Data;
+using Jaysbe.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jaysbe.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class ProductController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    private readonly ILogger<ProductController> _logger;
+
+    public ProductController(AppDbContext context, ILogger<ProductController> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Post(Product product)
+    {
+        _logger.LogInformation($"{nameof(Post)}, ");
+        
+        var entity = await _context.Products.AddAsync(product);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<Product> GetFirst()
+    {
+        var item = await _context.Products.FirstAsync();
+        return item;
+    }
+}
