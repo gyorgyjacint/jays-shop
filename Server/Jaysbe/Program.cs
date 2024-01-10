09da4699ae2af86e9cbf64fdc71682e36c3860c5
gyorgyjacint <jicant@gmail.com>
2024-01-10 17:39:52 +0100

    JsonSerializerOptions.ReferenceHandler value changed

--- Server/Jaysbe/Program.cs
+++ Server/Jaysbe/Program.cs
@@ -1,16 +1,21 @@
+using System.Text.Json.Serialization;
+using Jaysbe.Data;
 using Microsoft.EntityFrameworkCore;
-using AppContext = Jaysbe.Data.AppContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionStr = builder.Configuration["ConnectionString"];
-builder.Services.AddDbContext<AppContext>(options => options.UseSqlServer(connectionStr));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionStr));
 
 var app = builder.Build();
 
@@ -22,5 +27,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
