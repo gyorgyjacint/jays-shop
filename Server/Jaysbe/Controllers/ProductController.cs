94ea8363083d2ae2222e9affa5ddda9444c1bae0
gyorgyjacint <jicant@gmail.com>
2024-01-15 21:37:11 +0100

    refactor: ProductControllers Post action refactored to use ImageUploadHandler

--- Server/Jaysbe/Controllers/ProductController.cs
+++ Server/Jaysbe/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Jaysbe.Contracts;
 using Jaysbe.Data;
 using Jaysbe.Models;
+using Jaysbe.Services.File;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,78 +15,39 @@ public class ProductController : ControllerBase
     private readonly AppDbContext _context;
     private readonly ILogger<ProductController> _logger;
     private readonly IMapper _mapper;
+    private readonly IImageUploadHandler _imageUploadHandler;
 
-    public ProductController(AppDbContext context, IMapper mapper, ILogger<ProductController> logger)
+    public ProductController(AppDbContext context, IMapper mapper, IImageUploadHandler imageUploadHandler, ILogger<ProductController> logger)
     {
         _context = context;
         _logger = logger;
         _mapper = mapper;
+        _imageUploadHandler = imageUploadHandler;
     }
     
     [HttpPost]
     public async Task<IActionResult> Post([FromForm]ProductRequest product)
     {
         _logger.LogInformation($"{nameof(Post)}, {product.Name}");
-        IList<string> allowedFileExtensions = new List<string> { ".jpg", ".jpeg", ".png" };
-        int maxContentSize = 1024 * 1024 * 10; //10mb
         var mappedProduct = _mapper.Map<Product>(product);
         
-        if (product.Thumbnail.Length > 0)
+        var thumbnailResult = await _imageUploadHandler.AddImageAsync(product.Thumbnail);
+        if (!thumbnailResult.isSuccessful)
         {
-            var ext = product.Thumbnail.FileName.Substring(product.Thumbnail.FileName.LastIndexOf('.'));
-            var extension = ext.ToLower();
-            
-            if (!allowedFileExtensions.Contains(extension))
-                return BadRequest("Invalid file extension.");
-            if (product.Thumbnail.Length > maxContentSize)
-                return BadRequest("File too large");
-            
-            var path = "Pics";
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
-            var filePath = Path.Combine(path, Guid.NewGuid() + extension);
-
-            await using (var stream = System.IO.File.Create(filePath))
-            {
-                await product.Thumbnail.CopyToAsync(stream);
-            }
-            mappedProduct.ThumbnailUrl = filePath;
+            return BadRequest(thumbnailResult.errorMessages);
         }
+
+        mappedProduct.ThumbnailUrl = thumbnailResult.path;
 
         if (product.Pictures != null)
         {
-            if(product.Pictures != null && !product.Pictures.Any(f => 
-                   allowedFileExtensions.Contains(f.FileName.Substring(product.Thumbnail.FileName.LastIndexOf('.')).ToLower())))
-                return BadRequest("Invalid file extension.");
-            if (product.Pictures != null && product.Pictures.Any(f => f.Length > maxContentSize))
-                return BadRequest("File too large");
-            
-            foreach (var formFile in product.Pictures)
+            var picsResult = await _imageUploadHandler.AddImagesAsync(product.Pictures);
+            if (picsResult.paths.Any())
             {
-                var extension = product.Thumbnail.FileName.Substring(product.Thumbnail.FileName.LastIndexOf('.')).ToLower();
-                
-                if (formFile.Length > 0)
-                {
-                    var path = "Pics";
-                    
-                    if (!Directory.Exists(path))
-                        Directory.CreateDirectory(path);
-
-                    var filePath = Path.Combine(path, Guid.NewGuid() + extension);
-
-                    await using (var stream = System.IO.File.Create(filePath))
-                    {
-                        await formFile.CopyToAsync(stream);
-                    }
-
-                    mappedProduct.PicturesUrls ??= new List<string>();
-
-                    mappedProduct.PicturesUrls.Add(filePath);
-                }
+                mappedProduct.PicturesUrls = (IList<string>?)picsResult.paths;
             }
         }
-
+        
         await _context.Products.AddAsync(mappedProduct);
         _logger.LogInformation($"{mappedProduct.Name} product added to database");
         await _context.SaveChangesAsync();
