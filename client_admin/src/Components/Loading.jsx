e54486cc855d3c061030eca1e96255774a5f4ea5
gyorgyjacint <jicant@gmail.com>
2024-01-25 16:00:41 +0100

    change: Loading component design

--- client_admin/src/Components/Loading.jsx
+++ client_admin/src/Components/Loading.jsx
@@ -1,5 +1,17 @@
+import { Container } from '@mui/material';
 import './Loading.css';
 
 export default function Loading(){
-    return <div className="loader"></div>
+    const containerStyle = {
+        maxWidth: "90%",
+        margin: "auto",
+        marginTop: "150px",
+        display: "flex",
+        justifyContent:"center"
+    }
+    return (
+        <Container sx={containerStyle}>
+            <div className="loader" />
+        </Container>
+    );
 }
