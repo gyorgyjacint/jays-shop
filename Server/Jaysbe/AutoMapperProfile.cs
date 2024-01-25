d363a5ad779e42d894adabe986a3f77210db92e7
gyorgyjacint <jicant@gmail.com>
2024-01-25 14:21:59 +0100

    update: AutoMapperProfile updated to handle 2 new DTOs

--- Server/Jaysbe/AutoMapperProfile.cs
+++ Server/Jaysbe/AutoMapperProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Jaysbe.Contracts;
+using Jaysbe.Dtos;
 using Jaysbe.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Jaysbe;
 
@@ -9,5 +11,7 @@ public class AutoMapperProfile : Profile
     public AutoMapperProfile()
     {
         CreateMap<ProductRequest, Product>().ReverseMap();
+        CreateMap<Product, ProductResponse>();
+        CreateMap<IdentityUser, User<Guid>>();
     }
 }
