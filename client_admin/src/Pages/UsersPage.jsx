2a995ea1a8e2e2d39cd7c52e4a6a8e1434c9d5f8
gyorgyjacint <jicant@gmail.com>
2024-01-25 15:13:30 +0100

    feat: basic UsersPage implemented

--- client_admin/src/Pages/UsersPage.jsx
+++ client_admin/src/Pages/UsersPage.jsx
@@ -1,25 +1,51 @@
 import { useEffect, useState } from "react";
 import fetchDataAsync from "../Services/fetchDataAsync";
 import Loading from "../Components/Loading";
+import { Box, Container } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 
 export default function Users(){
-    const [products, setProducts] = useState(null);
+    const [users, setUsers] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const columns = [
+        { field: "id", headerName: "ID", width: 300 },
+        { field: "userName", headerName: "Username", width: 150 },
+        { field: "email", headerName: "Email", width: 170 },
+        { field: "emailConfirmed", headerName: "Email confirmed", width: 150 },
+        { field: "phoneNumber", headerName: "Phone number", width: 200 },
+        { field: "phoneNumberConfirmed", headerName: "Phone number confirmed", width: 200 }
+    ];
+
     useEffect(() => {
-        fetchDataAsync("/api/product/getall")
+        fetchDataAsync("/api/user/getall")
         .then(p => {
-            setProducts(p);
+            setUsers(p);
+            setLoading(false);
         })
     }, []);
 
     if (loading) {
-        return <Loading />
+        return (
+        <Container maxWidth="lg">
+            <Loading />
+        </Container>
+        );
     }
 
     return (
-    <div>
-
-    </div>
+    <Box maxWidth={"90%"} margin="auto" marginTop="30px" display="flex" justifyContent="center" justifyItems="center" justifySelf="center">
+        <DataGrid
+            rows={users}
+            columns={columns}
+            initialState={{
+                pagination: {
+                    paginationModel: {page: 0, pageSize: 15}
+                }
+            }}
+            pageSizeOptions={[5, 10, 15, 25, 50, 100]}
+            checkboxSelection
+        />
+    </Box>
     );
 }
