c7d3e0e6e869bdf4d75fca1642d56c792656ef26
gyorgyjacint <jicant@gmail.com>
2024-01-25 14:19:14 +0100

    add: User Dto

---
+++ Server/Jaysbe/Dtos/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Jaysbe.Dtos;
+
+public class User<TKey> where TKey : IEquatable<TKey>
+{
+    [PersonalData] 
+    public virtual TKey Id { get; set; } = default!;
+    [ProtectedPersonalData]
+    public virtual string? UserName { get; set; }
+    [ProtectedPersonalData]
+    public virtual string? Email { get; set; }
+    [PersonalData]
+    public virtual bool EmailConfirmed { get; set; }
+    [ProtectedPersonalData]
+    public virtual string? PhoneNumber { get; set; }
+    [PersonalData]
+    public virtual bool PhoneNumberConfirmed { get; set; }
+    
+}
