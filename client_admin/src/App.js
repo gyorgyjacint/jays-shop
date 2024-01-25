0fc0c5803d7c4a190ed4f44ce5eb42145b3c5245
gyorgyjacint <jicant@gmail.com>
2024-01-25 14:14:19 +0100

    change: router logic moved from index.js to App.js

--- client_admin/src/App.js
+++ client_admin/src/App.js
@@ -1,22 +1,65 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Layout from './Layout';
+import ErrorPage from './Pages/ErrorPage';
+import HomePage from './Pages/HomePage';
+
+import LoginPage from './Pages/LoginPage';
+import { ProtectedRoute } from './Components/ProtectedRoute';
+import ImgTest from './Pages/ImgTest';
+
 import './App.css';
+import { AuthProvider } from './Services/Authentication/AuthProvider';
+import Users from './Pages/UsersPage';
+import Products from './Pages/ProductsPage';
+import Orders from './Pages/OrdersPage';
+import { Container } from '@mui/material';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+      errorElement: <ErrorPage />
+    },
+    {
+      path: "/login",
+      element: (
+        <AuthProvider>
+          <LoginPage />
+        </AuthProvider>
+      )
+    },
+    {
+      path: "/home",
+      element: <HomePage />
+    },
+    {
+      path: "/users",
+      element: <Users />
+    },
+    {
+      path: "/products",
+      element: <Products />
+    },
+    {
+      path: "/orders",
+      element: <Orders />
+    },
+    {
+      path: "/imgtest",
+      element: (
+        <ImgTest />
+      )
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Layout />
+      <RouterProvider router={router} />
+    </Container>
   );
 }
 
--- client_admin/src/index.js
+++ client_admin/src/index.js
@@ -1,42 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-
 import App from './App';
-import Layout from './Layout';
-import ErrorPage from './Pages/ErrorPage';
-import HomePage from './Pages/HomePage';
-
 import './index.css';
-import LoginPage from './Pages/LoginPage';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-        <HomePage />
 
-    ),
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <LoginPage />
-  },
-  {
-    path: "/app",
-    element: (
-        <App />
-
-    )
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Layout />
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>
 );
 
