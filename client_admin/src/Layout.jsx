50a458187be83e4a1669960011d4cb518c2be483
gyorgyjacint <jicant@gmail.com>
2024-01-25 14:17:20 +0100

    change: Server status check added and Layout visibility is tied to login

--- client_admin/src/Layout.jsx
+++ client_admin/src/Layout.jsx
@@ -1,15 +1,59 @@
-import { Button, Container } from "@mui/material";
+import { Alert, Button, Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import isLoggedIn from "./Services/isLoggedIn";
+import fetchDataAsync from "./Services/fetchDataAsync";
 
 export default function Layout()
 {
+    const [serverStatus, setServerStatus] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(true);
+    const [mutation, setMutation] = useState(true);
+
+    const observer = new MutationObserver((mutationsList) => {
+        setMutation(!mutation);
+    });
+    observer.observe(document.getElementById("root"), { attributes: true, childList: true })
+
+    useEffect(() => {
+        isLoggedIn()
+        .then(isOk => {
+            setLoggedIn(isOk);
+            // Component is not part of a Route so useNavigate cannot be used here
+            if (!isOk && window.location.pathname !== "/login") {
+                window.location.replace("/login");
+            }
+        })
+    }, [mutation, loggedIn]);
+
+    useEffect(() => {
+        fetchDataAsync("/api/authtest/status")
+        .then(d => setServerStatus(d.ok))
+    }, [])
+
+    if (!serverStatus){
+        return <Alert severity="error">Server is down.</Alert>;
+    }
+
+    function fetchDummyAuth2() {
+        fetch("/api/dummy/dummyauth2");
+    }
+
+    const handleHomeBtn = () => { window.location.replace("/home") }
+    const handleUsersBtn = () => { window.location.replace("/users") }
+    const handleProductsBtn = () => { window.location.replace("/products") }
+    const handleOrdersBtn = () => { window.location.replace("/orders"); }
+
     return (
         <div>
-            <Container color="lightblue">
-                <Button variant="contained">Hello1</Button>
-                <Button variant="contained">Hello2</Button>
-                <Button variant="contained">Hello3</Button>
-                <Button variant="contained">Hello4</Button>
-            </Container>
+            {loggedIn && 
+                <Container color="lightblue">
+                    <Button variant="contained" onClick={handleHomeBtn}>Home</Button>
+                    <Button variant="contained" onClick={handleUsersBtn}>Users</Button>
+                    <Button variant="contained" onClick={handleProductsBtn}>Products</Button>
+                    <Button variant="contained" onClick={handleOrdersBtn}>Orders</Button>
+                </Container>
+            }
+            {!serverStatus && <Alert severity="error">Server is down.</Alert>}
         </div>
     );
 }
