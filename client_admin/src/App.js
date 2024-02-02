30578ee1fd426838212e263815f44f8b5a7e68ec
gyorgyjacint <jicant@gmail.com>
2024-02-02 14:28:42 +0100

    remove: unused imports

--- client_admin/src/App.js
+++ client_admin/src/App.js
@@ -5,7 +5,6 @@ import ErrorPage from './Pages/ErrorPage';
 import HomePage from './Pages/HomePage';
 
 import LoginPage from './Pages/LoginPage';
-import { ProtectedRoute } from './Components/ProtectedRoute';
 import ImgTest from './Pages/ImgTest';
 
 import './App.css';
@@ -13,7 +12,6 @@ import { AuthProvider } from './Services/Authentication/AuthProvider';
 import Users from './Pages/UsersPage';
 import Products from './Pages/ProductsPage';
 import Orders from './Pages/OrdersPage';
-import { Container } from '@mui/material';
 
 function App() {
 
@@ -56,10 +54,10 @@ function App() {
   ]);
 
   return (
-    <Container>
+    <>
       <Layout />
       <RouterProvider router={router} />
-    </Container>
+    </>
   );
 }
 
