403a920590239d18ed8b0f9f63619901c72b2577
gyorgyjacint <jicant@gmail.com>
2024-02-02 14:28:18 +0100

    change: isLoggedIn return value

--- client_admin/src/Services/isLoggedIn.js
+++ client_admin/src/Services/isLoggedIn.js
@@ -2,5 +2,5 @@ import fetchDataAsync from "./fetchDataAsync";
 
 export default function isLoggedIn(){
     return fetchDataAsync("api/AuthTest/AdminAuthorizedStatus")
-            .then(d => d.ok)
+            .then(d => d.status)
 }
