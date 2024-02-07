f5e45a8497a5079d8933135b05726b4803786a0b
gyorgyjacint <jicant@gmail.com>
2024-02-07 16:57:27 +0100

    feat: Images are removed from file system when required on Update action

--- Server/Jaysbe/Controllers/ProductController.cs
+++ Server/Jaysbe/Controllers/ProductController.cs
@@ -16,15 +16,15 @@ public class ProductController : ControllerBase
     private readonly AppDbContext _context;
     private readonly ILogger<ProductController> _logger;
     private readonly IMapper _mapper;
-    private readonly IImageUploadHandler _imageUploadHandler;
+    private readonly IProductImageHandler _productImageHandler;
 
-    public ProductController(AppDbContext context, IMapper mapper, IImageUploadHandler imageUploadHandler,
+    public ProductController(AppDbContext context, IMapper mapper, IProductImageHandler productImageHandler,
         ILogger<ProductController> logger)
     {
         _context = context;
         _logger = logger;
         _mapper = mapper;
-        _imageUploadHandler = imageUploadHandler;
+        _productImageHandler = productImageHandler;
     }
 
     [HttpPost]
@@ -34,7 +34,7 @@ public class ProductController : ControllerBase
         _logger.LogInformation($"Attempt to register product [{product.Name}]");
         var mappedProduct = _mapper.Map<Product>(product);
 
-        var thumbnailResult = await _imageUploadHandler.AddImageAsync(product.Thumbnail, ModelState);
+        var thumbnailResult = await _productImageHandler.AddImageAsync(product.Thumbnail, ModelState);
         if (!thumbnailResult.isSuccessful)
         {
             _logger.LogInformation(
@@ -46,11 +46,12 @@ public class ProductController : ControllerBase
 
         if (product.Pictures != null)
         {
-            var picsResult = await _imageUploadHandler.AddImagesAsync(product.Pictures, ModelState);
+            var picsResult = await _productImageHandler.AddImagesAsync(product.Pictures, ModelState);
             if (picsResult.paths.Any())
             {
                 mappedProduct.PicturesUrls = (IList<string>?)picsResult.paths;
             }
+
             _logger.LogInformation($"Saved [{picsResult.paths.Count()}] out of {product.Pictures.Count} images");
         }
 
@@ -82,14 +83,31 @@ public class ProductController : ControllerBase
     public async Task<ActionResult<Guid>> Update([FromBody] Product model)
     {
         _logger.LogInformation(nameof(Update));
-        var product = await _context.Products.FindAsync(model.ProductId);
+        var product = await _context.Products!.FindAsync(model.ProductId);
 
         if (product is null)
             return BadRequest();
 
+        if (product.PicturesUrls != null &&
+            product.PicturesUrls.Count > model.PicturesUrls?.Count)
+        {
+            var accessRoutesToRemove = (model.PicturesUrls == null || !model.PicturesUrls.Any()
+                ? product.PicturesUrls
+                : product.PicturesUrls.Where(u => !model.PicturesUrls.Contains(u)))
+                .ToArray();
+            
+            if (accessRoutesToRemove.Length > 0)
+            {
+                foreach (var accessRoute in accessRoutesToRemove)
+                {
+                    var result = _productImageHandler.RemoveFile(accessRoute);
+                }
+            }
+        }
+
         _context.Entry(product).CurrentValues.SetValues(model);
         await _context.SaveChangesAsync();
-        
+
         _logger.LogInformation($"Product with ID [{product.ProductId}] updated");
         return Ok(product.ProductId);
     }
@@ -105,7 +123,7 @@ public class ProductController : ControllerBase
             _logger.LogInformation($"Product with ID: [{id}] not found");
             return NotFound();
         }
-        
+
         return Ok(product);
     }
 
