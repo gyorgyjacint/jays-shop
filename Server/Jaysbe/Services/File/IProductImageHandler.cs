79b997336b9a603caf43992ced70a710751264b9
gyorgyjacint <jicant@gmail.com>
2024-02-07 16:55:18 +0100

    add: RemoveFile method to IProductImageHandler

--- Server/Jaysbe/Services/File/IProductImageHandler.cs
+++ Server/Jaysbe/Services/File/IProductImageHandler.cs
@@ -7,4 +7,5 @@ public interface IProductImageHandler
     public Task<(string path, bool isSuccessful, string[]? errorMessages)> AddImageAsync(IFormFile image,
         ModelStateDictionary modelState);
     public Task<(IEnumerable<string> paths, bool isSuccessful, string[]? errorMessages)> AddImagesAsync(IEnumerable<IFormFile> images, ModelStateDictionary modelState);
+    public bool RemoveFile(string accessRoute);
 }
