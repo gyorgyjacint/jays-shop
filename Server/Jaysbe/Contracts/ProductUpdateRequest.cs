ac76b877f5a2a707c197798157ba7f8f79ac0bd1
gyorgyjacint <jicant@gmail.com>
2024-02-08 12:37:04 +0100

    add: ProductUpdateRequest

---
+++ Server/Jaysbe/Contracts/ProductUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿using Jaysbe.Models;
+
+namespace Jaysbe.Contracts;
+
+public class ProductUpdateRequest : Product
+{
+    public IFormFile? ThumbnailNew { get; set; }
+    public ICollection<IFormFile>? PicturesNew { get; set; }
+}
