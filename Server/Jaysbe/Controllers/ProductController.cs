234b3972ff9b5be4abae78c19d6ac008c8a3ae09
gyorgyjacint <jicant@gmail.com>
2024-02-13 16:59:24 +0100

    refactor: ProductController refactored to use repository pattern

--- Server/Jaysbe/Controllers/ProductController.cs
+++ Server/Jaysbe/Controllers/ProductController.cs
@@ -1,12 +1,9 @@
-﻿using AutoMapper;
-using Jaysbe.Contracts;
-using Jaysbe.Data;
+﻿using Jaysbe.Contracts;
 using Jaysbe.Dtos;
 using Jaysbe.Models;
-using Jaysbe.Services.File;
+using Jaysbe.Services.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Jaysbe.Controllers;
 
@@ -14,52 +11,21 @@ namespace Jaysbe.Controllers;
 [Route("api/[controller]/[action]")]
 public class ProductController : ControllerBase
 {
-    private readonly AppDbContext _context;
     private readonly ILogger<ProductController> _logger;
-    private readonly IMapper _mapper;
-    private readonly IProductImageService _productImageService;
+    private readonly IProductRepository _repository;
 
-    public ProductController(AppDbContext context, IMapper mapper, IProductImageService productImageService,
-        ILogger<ProductController> logger)
+    public ProductController(ILogger<ProductController> logger, IProductRepository repository)
     {
-        _context = context;
         _logger = logger;
-        _mapper = mapper;
-        _productImageService = productImageService;
+        _repository = repository;
     }
 
     [HttpPost]
-    public async Task<IActionResult> Post([FromForm] ProductRequestDto product)
+    public async Task<IActionResult> Post([FromForm] ProductRequestDto model)
     {
-        _logger.LogInformation(nameof(Post) + $" attempts to register product [{product.Name}]");
-        var mappedProduct = _mapper.Map<Product>(product);
-
-        var thumbnailResult = await _productImageService.AddImageAsync(product.Thumbnail, ModelState);
-
-        if (thumbnailResult.isSuccessful)
-        {
-            mappedProduct.ThumbnailUrl = thumbnailResult.path;
-        }
-        else
-        {
-            _logger.LogInformation(
-                $"Register of product [{product.Name}] is unsuccessful, {nameof(thumbnailResult.isSuccessful)} is false");
-            return BadRequest(ModelState);
-        }
-
-        if (product.Pictures != null)
-        {
-            var picsResult = await _productImageService.AddImagesAsync(product.Pictures, ModelState);
-            if (picsResult.paths.Any())
-                mappedProduct.PicturesUrls = picsResult.paths.ToList();
-
-            _logger.LogInformation($"Saved [{picsResult.paths.Count()}] out of {product.Pictures.Count} images");
-        }
-
-        await _context.Products.AddAsync(mappedProduct);
-        await _context.SaveChangesAsync();
-        _logger.LogInformation($"{mappedProduct.Name} product added to database");
-        return Created("name", mappedProduct.Name);
+        _logger.LogInformation(nameof(Post) + $" attempts to register product [{model.Name}]");
+        var id = await _repository.Add(model, ModelState);
+        return id != null ? Created("name", id) : BadRequest();
     }
 
     [HttpDelete("{id}")]
@@ -67,116 +33,36 @@ public class ProductController : ControllerBase
     public async Task<ActionResult<Guid>> Delete([FromRoute] Guid id)
     {
         _logger.LogInformation(nameof(Delete));
-        var product = await _context.Products.FindAsync(id);
+        var result = await _repository.Remove(id);
 
-        if (product is null)
-            return NotFound();
-
-        _context.Remove(product);
-        await _context.SaveChangesAsync();
-
-        _logger.LogInformation($"Product with ID [{product.ProductId}] deleted");
-        return Ok(product.ProductId);
+        return result ? Ok(id) : NotFound();
     }
 
     [HttpPatch]
     [Authorize(Roles = "Admin")]
-    public async Task<ActionResult<(Guid id, int errorCount)>> Update([FromForm] ProductUpdateRequest model)
+    public async Task<ActionResult> Update([FromForm] ProductUpdateRequest model)
     {
         _logger.LogInformation(nameof(Update));
-        var product = await _context.Products!.FindAsync(model.ProductId);
+        var id = await _repository.Update(model, ModelState);
 
-        if (product is null)
+        if (id == null)
             return NotFound();
-
-        if (model.PicturesUrls != null &&
-            (model.PicturesUrls.Any(u => u == null!) || model.PicturesUrls.Any(u => u == "null")))
-        {
-            var control = model.PicturesUrls.Where(u => u != "null" && u != null!).ToArray();
-            model.PicturesUrls = control.Length > 0 ? control : null;
-        }
-
-        if (model.ThumbnailUrl == "null")
-            model.ThumbnailUrl = null;
-
-        if (product.ThumbnailUrl != null && (model.ThumbnailUrl == null || product.ThumbnailUrl.Length < 1))
-            _productImageService.RemoveFile(product.ThumbnailUrl);
-
-        if (product.PicturesUrls != null &&
-            product.PicturesUrls.Count > (model.PicturesUrls?.Count ?? 0))
-        {
-            var accessRoutesToRemove = (model.PicturesUrls == null || !model.PicturesUrls.Any()
-                    ? product.PicturesUrls
-                    : product.PicturesUrls.Where(u => !model.PicturesUrls.Contains(u)))
-                .ToArray();
-
-            if (accessRoutesToRemove.Length > 0)
-            {
-                foreach (var accessRoute in accessRoutesToRemove)
-                {
-                    var result = _productImageService.RemoveFile(accessRoute);
-                }
-            }
-        }
-
-        if (model.ThumbnailNew != null)
-        {
-            var result = await _productImageService.AddImageAsync(model.ThumbnailNew, ModelState);
-            if (result.isSuccessful)
-                model.ThumbnailUrl = result.path;
-            
-            if (result.isSuccessful && product.ThumbnailUrl != null)
-                _productImageService.RemoveFile(product.ThumbnailUrl);
-        }
-
-        if (model.PicturesNew != null || model.PicturesNew?.Count > 0)
-        {
-            var result = await _productImageService.AddImagesAsync(model.PicturesNew, ModelState);
-            if (result.paths.Any())
-            {
-                var paths = result.paths.Where(s => !string.IsNullOrEmpty(s));
-                model.PicturesUrls = (model.PicturesUrls != null
-                    ? model.PicturesUrls.Concat(paths)
-                    : paths).ToList();
-            }
-        }
-
-        _context.Entry(product).CurrentValues.SetValues(model);
-        await _context.SaveChangesAsync();
-
-        _logger.LogInformation($"Product with ID [{product.ProductId}] updated");
-        return Ok(new { id = product.ProductId, errorCount = ModelState.ErrorCount });
+        
+        return Ok(id);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetById(Guid id)
     {
         _logger.LogInformation(nameof(GetById));
-        var product = await _context.Products.FirstOrDefaultAsync(p => p.ProductId == id);
-
-        if (product is null)
-        {
-            _logger.LogInformation($"Product with ID: [{id}] not found");
-            return NotFound();
-        }
-
-        return Ok(product);
+        var product = await _repository.GetById(id);
+        return product != null ? Ok(product) : NotFound();
     }
 
     [HttpGet]
     public async Task<Product[]> GetAll()
     {
         _logger.LogInformation(nameof(GetAll));
-
-        if (_context.Products == null)
-        {
-            _logger.LogInformation($"{nameof(GetAll)}: no products found");
-            return Array.Empty<Product>();
-        }
-
-        var products = await _context.Products.ToArrayAsync();
-        _logger.LogInformation($"{nameof(GetAll)}: {products.Length} products found");
-
-        return products;
+        return await _repository.GetAll();
     }
 }
