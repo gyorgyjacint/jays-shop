2446caefca94532aace49d73bed1f3648afd37d2
gyorgyjacint <jicant@gmail.com>
2024-02-14 10:55:34 +0100

    add: ICategoryRepository and its implementation added

---
+++ Server/Jaysbe/Services/Repository/CategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using Jaysbe.Data;
+using Jaysbe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jaysbe.Services.Repository;
+
+public class CategoryRepository : ICategoryRepository
+{
+    
+    private readonly AppDbContext _context;
+    private readonly ILogger<ProductRepository> _logger;
+
+    public CategoryRepository(AppDbContext context, ILogger<ProductRepository> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+    
+    public async Task<Category[]> GetAll()
+    {
+        var result = await _context.Categories.ToArrayAsync();
+        _logger.LogInformation($"{result.Length} categories found");
+        return result;
+    }
+
+    public async Task<Guid?> Add(Category model)
+    {
+        var entity = await _context.Categories.AddAsync(model);
+        await _context.SaveChangesAsync();
+
+        _logger.LogInformation($"{nameof(Add)}, Category entity added to database.");
+        return entity.Entity.CategoryId;
+    }
+
+    public async Task<Guid?> Delete(Guid id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+
+        if (category == null)
+        {
+            _logger.LogInformation($"Category with ID [{id}] not found");
+            return null;
+        }
+
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+        _logger.LogInformation($"Category with ID [{id}] removed");
+        
+        return category.CategoryId;
+    }
+}
---
+++ Server/Jaysbe/Services/Repository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Jaysbe.Models;
+
+namespace Jaysbe.Services.Repository;
+
+public interface ICategoryRepository
+{
+    public Task<Category[]> GetAll();
+    public Task<Guid?> Add(Category model);
+    public Task<Guid?> Delete(Guid id);
+}
