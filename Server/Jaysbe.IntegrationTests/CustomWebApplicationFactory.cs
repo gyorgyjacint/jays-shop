ec72edadf2e459edc4608edc4476759db455de89
gyorgyjacint <jicant@gmail.com>
2024-02-20 10:17:10 +0100

    add: CustomWebApplicationFactory with in-memory database

---
+++ Server/Jaysbe.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,28 @@
+﻿using Jaysbe.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jaysbe.IntegrationTests;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var dbContextDescriptor =
+                services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+            services.Remove(dbContextDescriptor!);
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("testDatabase");
+            });
+        });
+        
+        builder.UseEnvironment("Development");
+    }
+}
