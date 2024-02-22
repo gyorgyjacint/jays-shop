18c22eb99859e4de732166846495583c023bde1e
gyorgyjacint <jicant@gmail.com>
2024-02-22 18:39:18 +0100

    add: More tests for AuthController

--- Server/Jaysbe.IntegrationTests/Controllers/AuthControllerTests.cs
+++ Server/Jaysbe.IntegrationTests/Controllers/AuthControllerTests.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 using Jaysbe.Contracts;
@@ -7,33 +6,33 @@ using Jaysbe.Contracts;
 namespace Jaysbe.IntegrationTests.Controllers;
 
 [Collection("auth")]
-public class AuthControllerTests : IClassFixture<CustomWebApplicationFactory<Jaysbe.Program>>
+public class AuthControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
-    private readonly CustomWebApplicationFactory<Jaysbe.Program> _applicationFactory;
+    private readonly HttpClient _client;
 
     public AuthControllerTests(CustomWebApplicationFactory<Program> applicationFactory)
     {
-        _applicationFactory = applicationFactory;
+        _client = applicationFactory.CreateClient();
     }
+
+    #region Register
 
     [Theory]
     [InlineData("/api/auth/register")]
     public async Task Register_IsSuccessful(string url)
     {
         // Arrange
-        var client = _applicationFactory.CreateClient();
         var regData = new RegistrationRequest("test1@test.com", "test1", "test1pw");
         var content = JsonContent.Create(regData);
         
         // Act
-        var regEvent = await client.PostAsync(url, content);
+        var responseMessage = await _client.PostAsync(url, content);
 
         // Assert
-        
         Assert.Multiple(() =>
         {
-            Assert.True(regEvent.IsSuccessStatusCode);
-            Assert.True(regEvent.StatusCode == HttpStatusCode.Created);
+            Assert.True(responseMessage.IsSuccessStatusCode);
+            Assert.True(responseMessage.StatusCode == HttpStatusCode.Created);
         });
     }
     
@@ -42,7 +41,6 @@ public class AuthControllerTests : IClassFixture<CustomWebApplicationFactory<Jay
     public async Task Register_Returns_Correct_Data_OnSuccessful(string url)
     {
         // Arrange
-        var client = _applicationFactory.CreateClient();
         var regData = new RegistrationRequest("test2@test.com", "test2", "test2pw");
         var content = JsonContent.Create(regData);
         var jsonOptions = new JsonSerializerOptions
@@ -51,18 +49,172 @@ public class AuthControllerTests : IClassFixture<CustomWebApplicationFactory<Jay
         };
             
         // Act
-        var regEvent = await client.PostAsync(url, content);
-        var json = await regEvent.Content.ReadAsStringAsync();
+        var responseMessage = await _client.PostAsync(url, content);
+        var json = await responseMessage.Content.ReadAsStringAsync();
         var responseData = JsonSerializer.Deserialize<RegistrationResponse>(json, jsonOptions);
 
         // Assert
-        
         Assert.Multiple(() =>
         {
-            Assert.True(regEvent.IsSuccessStatusCode);
-            Assert.True(regEvent.StatusCode == HttpStatusCode.Created);
+            Assert.True(responseMessage.IsSuccessStatusCode);
+            Assert.True(responseMessage.StatusCode == HttpStatusCode.Created);
             Assert.Equivalent(regData.Email, responseData?.Email);
             Assert.Equivalent(regData.UserName, responseData?.UserName);
         });
     }
+    
+    [Theory]
+    [InlineData("/api/auth/register")]
+    public async Task Register_Returns_Error_With_Unavailable_Email(string url)
+    {
+        // Arrange
+        var regData = new RegistrationRequest("test1@test.com", "test1", "test1pw");
+        var content = JsonContent.Create(regData);
+            
+        // Act
+        var responseMessage = await _client.PostAsync(url, content);
+
+        // Assert
+        Assert.False(responseMessage.IsSuccessStatusCode);
+    }
+    
+    [Theory]
+    [InlineData("/api/auth/register")]
+    public async Task Register_Returns_Error_With_Unavailable_UserName(string url)
+    {
+        // Arrange
+        var regData = new RegistrationRequest("test4@test.com", "test1", "test4pw");
+        var content = JsonContent.Create(regData);
+            
+        // Act
+        var responseMessage = await _client.PostAsync(url, content);
+
+        // Assert
+        Assert.False(responseMessage.IsSuccessStatusCode);
+    }
+    
+    [Theory]
+    [InlineData("/api/auth/register")]
+    public async Task Register_Returns_Error_With_Incorrect_Email(string url)
+    {
+        // Arrange
+        var regData = new RegistrationRequest("test1_at_test_dot_com", "test1", "test1pw");
+        var content = JsonContent.Create(regData);
+            
+        // Act
+        var responseMessage = await _client.PostAsync(url, content);
+
+        // Assert
+        Assert.False(responseMessage.IsSuccessStatusCode);
+    }
+    
+    [Theory]
+    [InlineData("/api/auth/register")]
+    public async Task Register_Returns_Error_With_Incorrect_Password(string url)
+    {
+        // Arrange
+        var regData = new RegistrationRequest("test5@test.com", "test5", "12345");
+        var content = JsonContent.Create(regData);
+            
+        // Act
+        var responseMessage = await _client.PostAsync(url, content);
+
+        // Assert
+        Assert.False(responseMessage.IsSuccessStatusCode);
+    }
+    
+    #endregion
+
+    #region Authenticate
+    
+    [Theory]
+    [InlineData("/api/auth/authenticate")]
+    public async Task Authenticate_Returns_SetCookie_Headers(string url)
+    {
+        // Arrange
+        var authData = new AuthRequest("test1@test.com", "test1pw");
+        var content = JsonContent.Create(authData);
+            
+        // Act
+        var responseMessage = await _client.PostAsync(url, content);
+
+        // Assert
+        var setCookieHeaders = responseMessage.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value;
+        Assert.NotEmpty(setCookieHeaders);
+    }
+    
+    [Theory]
+    [InlineData("/api/auth/authenticate")]
+    public async Task Authenticate_Returns_Email_And_UserName(string url)
+    {
+        // Arrange
+        var authData = new AuthRequest("test1@test.com", "test1pw");
+        var content = JsonContent.Create(authData);
+        var jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+            
+        // Act
+        var responseMessage = await _client.PostAsync(url, content);
+        var json = await responseMessage.Content.ReadAsStringAsync();
+        var responseData = JsonSerializer.Deserialize<AuthResponse>(json, jsonOptions);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.Equivalent(authData.Email, responseData?.Email);
+            Assert.Equivalent("test1", responseData?.UserName);
+        });
+    }
+    
+    [Theory]
+    [InlineData("/api/auth/authenticate")]
+    [InlineData("/api/auth/authenticateadmin")]
+    public async Task Authenticate_Returns_Error_With_Invalid_Email(string url)
+    {
+        // Arrange
+        var authData = new AuthRequest("test1test.com", "test1pw");
+        var content = JsonContent.Create(authData);
+            
+        // Act
+        var responseMessage = await _client.PostAsync(url, content);
+
+        // Assert
+        Assert.False(responseMessage.IsSuccessStatusCode);
+    }
+    
+    [Theory]
+    [InlineData("/api/auth/authenticate")]
+    public async Task Authenticate_Returns_Error_With_Invalid_Password(string url)
+    {
+        // Arrange
+        var authData = new AuthRequest("test1@test.com", "invalid password");
+        var content = JsonContent.Create(authData);
+            
+        // Act
+        var responseMessage = await _client.PostAsync(url, content);
+
+        // Assert
+        Assert.False(responseMessage.IsSuccessStatusCode);
+    }
+    
+    // admin
+    [Theory]
+    [InlineData("/api/auth/authenticateadmin")]
+    public async Task AuthenticateAdmin_Returns_Error_With_Invalid_Password(string url)
+    {
+        // Arrange
+        var authData = new AuthRequest("admin@admin.com", "invalid password");
+        var content = JsonContent.Create(authData);
+            
+        // Act
+        var responseMessage = await _client.PostAsync(url, content);
+
+        // Assert
+        Assert.False(responseMessage.IsSuccessStatusCode);
+    }
+    
+    #endregion
+
 }
