7369e7af6a9dea8ead175ebb098169d647e1e05e
gyorgyjacint <jicant@gmail.com>
2024-02-27 18:40:33 +0100

    add: ProductTestRequest model for tests

---
+++ Server/Jaysbe.IntegrationTests/Models/ProductTestRequest.cs
@@ -0,0 +1,22 @@
+﻿using Jaysbe.Models;
+
+namespace Jaysbe.IntegrationTests.Models;
+
+public class ProductTestRequest
+{
+    public string Name { get; set; }
+    public string? Brand { get; set; }
+    
+    public decimal Price { get; set; }
+    public decimal? DiscountPrice { get; set; }
+    public UInt16 Quantity { get; set; }
+
+    public string Description { get; set; }
+    public string? Color { get; set; }
+    public IEnumerable<ProductDescOption>? ProductDescriptions { get; set; }
+    
+    public BinaryData Thumbnail { get; set; }
+    public ICollection<BinaryData>? Pictures { get; set; }
+    
+    public Category? Category { get; set; }
+}
