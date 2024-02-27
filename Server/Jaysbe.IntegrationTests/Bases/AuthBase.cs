bad0375c851f86da1df2dae6f874999b074b085c
gyorgyjacint <jicant@gmail.com>
2024-02-27 12:25:20 +0100

    refactor: AuthBase, readability

--- Server/Jaysbe.IntegrationTests/Bases/AuthBase.cs
+++ Server/Jaysbe.IntegrationTests/Bases/AuthBase.cs
@@ -7,20 +7,30 @@ public abstract class AuthBase
 {
     protected readonly CustomWebApplicationFactory<Program> ApplicationFactory;
     protected readonly HttpClient Client;
-    private readonly string Token;
-    
+
     protected AuthBase(CustomWebApplicationFactory<Program> applicationFactory, string email, string password)
     {
         ApplicationFactory = applicationFactory;
         Client = applicationFactory.CreateClient();
-        var content = JsonContent.Create(new AuthRequest(email, password));
-        var response = Client.PostAsync("/api/auth/authenticate", content);
-        response.Wait();
+
+        var response = GetAuthResponse(email, password);
+        var token = ExtractToken(response);
         
+        Client.DefaultRequestHeaders.Add("Authorization", new []{ "Bearer " + token });
+    }
+
+    private Task<HttpResponseMessage> GetAuthResponse(string email, string password)
+    {
+        var content = JsonContent.Create(new AuthRequest(email, password));
+        return Client.PostAsync("/api/auth/authenticate", content);
+    }
+
+    private string ExtractToken(Task<HttpResponseMessage> response)
+    {
+        response.Wait();
         var authHeader = response.Result.Headers
             .FirstOrDefault(header => header.Key == "Set-Cookie").Value;
         
-        Token = authHeader.FirstOrDefault().Split("=")[1];
-        Client.DefaultRequestHeaders.Add("Authorization", new []{ "Bearer " + Token });
+        return authHeader.FirstOrDefault().Split("=")[1];
     }
 }
