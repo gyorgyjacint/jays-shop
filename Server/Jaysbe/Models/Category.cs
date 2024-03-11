cf73489222f0de9277b111a19b4a8a9c0848abd3
gyorgyjacint <jicant@gmail.com>
2024-03-11 15:36:29 +0100

    add: Parent to Category, migration

--- Server/Jaysbe/Models/Category.cs
+++ Server/Jaysbe/Models/Category.cs
@@ -11,5 +11,30 @@ public class Category
     
     [MaxLength(70)]
     public string Name { get; set; }
-    public IEnumerable<SubCategory>? SubCategories { get; set; }
+    
+    [ForeignKey("Category")]
+    public Guid? ParentId { get; set; }
+    public Category? Parent { get; set; }
+    
+    /*
+    [NotMapped]
+    public string[]? Parents
+    {
+        get
+        {
+            var q = new Stack<Category>();
+            Category examinable = this;
+            
+            while (examinable.ParentId != null && examinable.Parent != null)
+            {
+                q.Push(examinable.Parent);
+                examinable = examinable.Parent;
+            }
+
+            var res = q.Select(c => c.Name);
+            
+            return q.Count > 0 ? res.ToArray() : null;
+        }
+    }
+    //public IEnumerable<SubCategory>? SubCategories { get; set; }*/
 }
