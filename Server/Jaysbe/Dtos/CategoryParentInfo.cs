343aede9a76151f95604ed09a91512f2a23b12b7
gyorgyjacint <jicant@gmail.com>
2024-03-12 19:39:10 +0100

    rename: CategoryParentInfo to CategoryBase

--- Server/Jaysbe/Dtos/CategoryParentInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Jaysbe.Dtos;
-
-public class CategoryParentInfo
-{
-    public Guid? CategoryId { get; init; }
-    public string Name { get; set; }
-}
---
+++ Server/Jaysbe/Models/CategoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jaysbe.Models;
+
+[NotMapped]
+public class CategoryBase
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid? CategoryId { get; init; }
+    [MaxLength(70)]
+    public string Name { get; set; }
+}
