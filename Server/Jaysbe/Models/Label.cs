c7c502c9fa8b556b6bdca801466ee7bf88507de3
gyorgyjacint <jicant@gmail.com>
2024-03-27 14:38:13 +0100

    add: Label database model

---
+++ Server/Jaysbe/Models/Label.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jaysbe.Models;
+
+public class Label
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid LabelId { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+}
